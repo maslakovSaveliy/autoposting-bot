2389da4cf11016ea0ea9010648bda4118bd58b5f
maslakovSaveliy <saveliymaslakov969@gmail.com>
2024-02-28 14:23:23 +0300

    reGenerateFn

--- src/openai.js
+++ src/openai.js
@@ -7,30 +7,49 @@ class OpenAIService {
     USER: "user",
   };
 
+  chat = [];
+
   constructor(apiKey) {
     this.openai = new OpenAI({
       apiKey,
     });
   }
 
-  async generatePost(post) {
+  async reGeneratePost() {
     try {
+      this.chat.push({
+        role: this.roles.USER,
+        content: `Перепиши этот пост`,
+      });
       const res = await this.openai.chat.completions.create({
         model: "gpt-4",
-        messages: [
-          {
-            role: this.roles.USER,
-            content: "Привет",
-          },
-          {
-            role: this.roles.ASSISTANT,
-            content: "Привет! Как я могу помочь тебе сегодня?",
-          },
-          {
-            role: this.roles.USER,
-            content: `Тебе нужно написать новостной пост. Он должен состоять из заголовка и текста. У меня уже есть новость которую ты должен переписать: ${post.title}.\n${post.description}. Ответь мне уже готовым новостным постом`,
-          },
-        ],
+        messages: [...this.chat],
+      });
+      return res.choices[0].message.content;
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
+  async generatePost(post) {
+    try {
+      this.chat = [
+        {
+          role: this.roles.USER,
+          content: "Привет",
+        },
+        {
+          role: this.roles.ASSISTANT,
+          content: "Привет! Как я могу помочь тебе сегодня?",
+        },
+        {
+          role: this.roles.USER,
+          content: `Тебе нужно написать новостной пост. У меня уже есть новость которую ты должен переписать:\n${post.title}.\n${post.description}.\nОтветь мне уже готовым новостным постом`,
+        },
+      ];
+      const res = await this.openai.chat.completions.create({
+        model: "gpt-4",
+        messages: this.chat,
       });
       return res.choices[0].message.content;
     } catch (e) {
