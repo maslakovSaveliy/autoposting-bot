a61d877698dfa14e97f38cebda67d33ef76215c0
maslakovSaveliy <saveliymaslakov969@gmail.com>
2024-02-28 14:00:22 +0300

    refactoring

--- src/pars.js
+++ src/pars.js
@@ -1,18 +1,9 @@
 import puppeteer from "puppeteer";
 
 async function getPost() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
   await page.goto("https://habr.com/ru/news/");
-
-  // const post = await page.evaluate(() => {
-  //   const title = document.getElementsByClassName("tm-title_h2");
-  //   const description = document.getElementsByClassName(
-  //     "article-formatted-body"
-  //   );
-
-  //   return { title: title[0].innerText, description: description[0].innerText };
-  // });
 
   const post = await page.evaluate(() => {
     const postContainer = document.querySelectorAll(".tm-articles-list__item");
