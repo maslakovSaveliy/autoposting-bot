1d34748507fe27ef30d09d04480ad82efd329d49
maslakovSaveliy <saveliymaslakov969@gmail.com>
2024-03-11 14:06:44 +0300

    add habr in sources

--- src/pars.js
+++ src/pars.js
@@ -28,44 +28,89 @@ class Pars {
   }
 
   async getNewPost() {
-    const res = await axios
-      .get("https://mashable.com/category/social-good")
-      .then((res) => {
-        const curPage = res.data;
-        const dom = new JSDOM(curPage);
-        const elements = dom.window.document.querySelector(
-          "body > main > section > section > div"
-        );
+    let post = {};
+    const rndmN = Math.round(Math.random());
 
-        const randomNum = Math.floor(Math.random() * 25) * 3;
+    switch (rndmN) {
+      case 0:
+        const habrPost = await axios
+          .get("https://habr.com/ru/flows/marketing/articles/")
+          .then((res) => {
+            const randomNum = Math.floor(Math.random() * 10);
+            const curPage = res.data;
+            const dom = new JSDOM(curPage);
 
-        const article = elements
-          .getElementsByClassName("w-full")
-          [randomNum].querySelectorAll("div > div > a")[0];
+            const postContainer = dom.window.document.querySelectorAll(
+              ".tm-articles-list__item"
+            );
 
-        const description = elements
-          .getElementsByClassName("w-full")
-          [randomNum].getElementsByClassName("hidden")[0];
+            const titleElement =
+              postContainer[randomNum].querySelector(".tm-title_h2");
 
-        const url = elements
-          .getElementsByClassName("w-full")
-          [randomNum].querySelectorAll("div > a")[0]
-          .getAttribute("href");
+            const descriptionElement = postContainer[randomNum].querySelector(
+              ".article-formatted-body"
+            );
 
-        const img = elements
-          .getElementsByClassName("w-full")
-          [randomNum].querySelectorAll("div > a > div")[0]
-          .querySelector("img")
-          .getAttribute("src");
+            const url = postContainer[randomNum]
+              .querySelector(".tm-article-snippet__readmore")
+              .getAttribute("href");
 
-        return {
-          title: article.textContent.trim(),
-          description: description.textContent,
-          url: "https://mashable.com" + url,
-          media: img,
-        };
-      });
-    return res;
+            const img = postContainer[randomNum].querySelector(
+              ".tm-article-snippet__lead-image"
+            );
+            return {
+              title: titleElement.textContent,
+              description: descriptionElement.textContent,
+              url: "https://habr.com" + url,
+              media: img ? img.getAttribute("src") : null,
+            };
+          });
+
+        post = habrPost;
+        break;
+
+      case 1:
+        const res = await axios
+          .get("https://mashable.com/category/social-good")
+          .then((res) => {
+            const curPage = res.data;
+            const dom = new JSDOM(curPage);
+            const elements = dom.window.document.querySelector(
+              "body > main > section > section > div"
+            );
+
+            const randomNum = Math.floor(Math.random() * 25) * 3;
+
+            const article = elements
+              .getElementsByClassName("w-full")
+              [randomNum].querySelectorAll("div > div > a")[0];
+
+            const description = elements
+              .getElementsByClassName("w-full")
+              [randomNum].getElementsByClassName("hidden")[0];
+
+            const url = elements
+              .getElementsByClassName("w-full")
+              [randomNum].querySelectorAll("div > a")[0]
+              .getAttribute("href");
+
+            const img = elements
+              .getElementsByClassName("w-full")
+              [randomNum].querySelectorAll("div > a > div")[0]
+              .querySelector("img")
+              .getAttribute("src");
+
+            return {
+              title: article.textContent.trim(),
+              description: description.textContent,
+              url: "https://mashable.com" + url,
+              media: img,
+            };
+          });
+        post = res;
+        break;
+    }
+    return post;
   }
 }
 
