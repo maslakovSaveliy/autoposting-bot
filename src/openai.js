cdcd6268b9244cdac3c2575f946daf120d1d2039
maslakovSaveliy <saveliymaslakov969@gmail.com>
2024-03-26 07:22:02 +0300

    msg too long fix

--- src/openai.js
+++ src/openai.js
@@ -44,6 +44,22 @@ class OpenAIService {
 
   async generatePost(post) {
     try {
+      const max_size = 4096;
+      var messageString = post.description;
+
+      var amount_sliced = messageString.length / max_size;
+      var start = 0;
+      var end = max_size;
+      var message;
+      var messagesArray = [];
+
+      for (let i = 0; i < amount_sliced; i++) {
+        message = messageString.slice(start, end);
+        messages.push(message);
+        start = start + max_size;
+        end = end + max_size;
+      }
+
       this.chat = [
         {
           role: this.roles.USER,
@@ -55,7 +71,17 @@ class OpenAIService {
         },
         {
           role: this.roles.USER,
-          content: `Ты популярный новостной журналист. Тебе нужно написать новостной пост в свои соц сети, в котором ты будешь обсуждать и рассказывать про новость. У меня уже есть новость для тебя:\n${post.title}.\n${post.description}.\nОтветь мне уже готовым новостным постом. Уникальность должна быть 90%. Пиши на русском языке. Должно быть меньше 1000 символов. ${this.prompt}`,
+          content:
+            `Ты популярный новостной журналист. Тебе нужно написать новостной пост в свои соц сети, в котором ты будешь обсуждать и рассказывать про новость. У меня уже есть новость для тебя:` +
+            `\n` +
+            `${post.title}.`,
+        },
+        ...messagesArray.map((item) => {
+          return { role: this.roles.USER, content: item + `\n` };
+        }),
+        {
+          role: this.roles.USER,
+          content: `Ответь мне уже готовым новостным постом. Уникальность должна быть 90%. Пиши на русском языке. Должно быть меньше 1000 символов. ${this.prompt}`,
         },
       ];
       const res = await this.openai.chat.completions.create({
